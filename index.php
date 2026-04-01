7745925865930ac9bcc9a527510e6f67fbc0d681
Ichsan-Ghazali <i43250004@student.polije.ac.id>
2026-04-01 15:28:11 +0700

    index.php

--- index.php
+++ index.php
@@ -1,27 +1,41 @@
 <?php
 require_once 'TransferBank.php';
-require_once 'EWallet.php';
-require_once 'QRIS.php';
+require_once 'Ewallet.php';
+require_once 'Qris.php';
+require_once 'VirtualAccount.php';
 
-// objek
+// Inisialisasi Objek dengan nominal masing-masing
 $transfer = new TransferBank(100000);
-$ewallet = new EWallet(50000);
-$qris = new QRIS(75000);
+$ewallet  = new Ewallet(500000);
+$qris     = new Qris(750000);
+// Menggunakan huruf kecil di awal: $virtualAccount
+$virtualAccount = new VirtualAccount(250000);
 
-// output
+// Menampilkan Output Transfer Bank
+echo "<b>Transfer Bank:</b><br>";
 echo $transfer->prosesPembayaran();
 echo "<br>";
 echo $transfer->cetakStruk();
+echo "<br><br>";
 
-echo "<hr>";
-
+// Menampilkan Output E-Wallet
+echo "<b>E-Wallet:</b><br>";
 echo $ewallet->prosesPembayaran();
 echo "<br>";
 echo $ewallet->cetakStruk();
+echo "<br><br>";
 
-echo "<hr>";
-
+// Menampilkan Output QRIS
+echo "<b>QRIS:</b><br>";
 echo $qris->prosesPembayaran();
 echo "<br>";
 echo $qris->cetakStruk();
+echo "<br><br>";
+
+// Menampilkan Output Virtual Account
+// Pastikan memanggil variabel yang SAMA: $virtualAccount (v kecil)
+echo "<b>Virtual Account:</b><br>";
+echo $virtualAccount->prosesPembayaran();
+echo "<br>";
+echo $virtualAccount->cetakStruk();
 ?>
